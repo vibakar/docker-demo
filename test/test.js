8d6327b7b3dadbf7fc7ae42375dfda2c06aed6f6
vibakar <vibakar23@gmail.com>
2018-03-10 22:25:11 +0530

    test file updated to make the test case fail

--- test/test.js
+++ test/test.js
@@ -4,5 +4,9 @@ describe('Array', function() {
     it('should return -1 when the value is not present', function() {
       assert.equal(-1, [1,2,3].indexOf(4));
     });
+
+    it('should return the correct index', function() {
+      assert.equal(1, [1,2,3].indexOf(3));
+    });
   });
 });
